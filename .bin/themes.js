c250eb1deee595d12559bd719147909af2fa91b9
Zdravko Dimov <thewebmasterp@gmail.com>
2023-02-07 21:32:57 +0000

    Clean up...

--- .bin/themes.js
+++ .bin/themes.js
@@ -18,45 +18,7 @@ if (args[0]) {
     console.log(`Unexpected argument "${args[0]}"`)
     process.exit()
   }
-  // Process input
-  //exec(`notify-send ${args[0]}`)
-  //switch(args[0]) {}
   const configModules = '/home/master/config-modules/index'
-  /* const run = async () => {
-    try {
-      await execAsync('killall rofi')
-      let stdin, stdout
-      ;[stdin, stdout] = await execAsync(
-        `node ${configModules} --config-modules-dir ~/.config-modules --entry 'dunst-notification' --module ${
-          args[0]
-        }`
-      )
-      console.log(stdin, stdout)
-      console.log('you kidding me')
-      await execAsync(
-        `node ${configModules} --config-modules-dir ~/.config-modules --entry 'gtk-widget-toolkit' --module ${
-          args[0]
-        }`
-      )
-      await execAsync(
-        `node ${configModules} --config-modules-dir ~/.config-modules --entry 'i3-wm' --module ${
-          args[0]
-        }`
-      )
-      await execAsync(
-        `node ${configModules} --config-modules-dir ~/.config-modules --entry 'rofi-menu' --module ${
-          args[0]
-        }`
-      )
-    } catch (e) {
-      throw e
-    } finally {
-      return 'done'
-    }
-  }
-  run().then(data => {
-    console.log(data)
-  })*/
 
   exec(
     `node ${configModules} --entry 'alacritty-terminal' --module ${args[0]}.yml`
